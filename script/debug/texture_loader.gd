98cd7df6e262bd8f1f516f03b9447b68f65301d9
TechMan06 <132509305+TechMan06@users.noreply.github.com>
2025-01-18 22:42:26 -0300

    TextureLoader is now working.

--- script/debug/texture_loader.gd
+++ script/debug/texture_loader.gd
@@ -5,14 +5,26 @@ class_name TextureLoader
 @export var protected_texture_path: String = ""
 
 func _ready() -> void:
-	if FileAccess.file_exists(protected_texture_path):
-		if get_parent() is MeshInstance3D:
-			if get_parent().get_surface_override_material(0).get_shader_parameter("albedoTex") != null:
-				get_parent().get_surface_override_material(0).set_shader_parameter(
-															"albedoTex", 
-															load(protected_texture_path)
-														)
+	if !Engine.is_editor_hint():
+		if FileAccess.file_exists(protected_texture_path):
+			if get_parent() is MeshInstance3D:
+				if get_parent().get_surface_override_material(0).get_shader_parameter("albedoTex") != null:
+					get_parent().get_surface_override_material(0).set_shader_parameter(
+																"albedoTex", 
+																load(protected_texture_path)
+															)
+				else:
+					printerr("Texture file \"" + protected_texture_path + "\" not found!")
 			else:
-				printerr("Texture file \"" + protected_texture_path + "\" not found!")
+				printerr("Parent is not NiftyMesh or MeshInstance3D!")
 		else:
-			printerr("Parent is not NiftyMesh or MeshInstance3D!")
+			if get_parent() is MeshInstance3D:
+				if get_parent().get_surface_override_material(0).get_shader_parameter("albedoTex") != null:
+					get_parent().get_surface_override_material(0).set_shader_parameter(
+																"albedoTex", 
+																load(protected_texture_path)
+															)
+				else:
+					printerr("Texture file \"" + protected_texture_path + "\" not found!")
+			else:
+				printerr("Parent is not NiftyMesh or MeshInstance3D!")
