e0eb1b7292adb926664cae53bab4628eaaf19809
TechMan06 <132509305+TechMan06@users.noreply.github.com>
2025-01-28 22:04:28 -0300

    Hopefully this fixes the bucket

--- script/object/interactive/bucket.gd
+++ script/object/interactive/bucket.gd
@@ -9,12 +9,11 @@ func _on_body_entered(body):
 		touched_body = body
 		
 		if body.direction == 0 || body.direction == 3:
-			print("WORK!!!!!")
-			set_axis_lock(PhysicsServer3D.BODY_AXIS_LINEAR_X, false)
-			set_axis_lock(PhysicsServer3D.BODY_AXIS_LINEAR_Z, true)
-		else:
 			set_axis_lock(PhysicsServer3D.BODY_AXIS_LINEAR_X, true)
 			set_axis_lock(PhysicsServer3D.BODY_AXIS_LINEAR_Z, false)
+		else:
+			set_axis_lock(PhysicsServer3D.BODY_AXIS_LINEAR_X, false)
+			set_axis_lock(PhysicsServer3D.BODY_AXIS_LINEAR_Z, true)
 
 
 func _on_body_exited(body):
@@ -22,10 +21,6 @@ func _on_body_exited(body):
 	touched_body = null
 
 
-func _integrate_forces(state):
+func _physics_process(_delta):
 	if touched_body != null:
-		if touched_body.move_and_slide(): # true if collided
-			for i in touched_body.get_slide_collision_count():
-				var col = touched_body.get_slide_collision(i)
-				if col.get_collider() is RigidBody2D:
-					col.get_collider().apply_force(col.get_normal() * -touched_body.velocity)
+		apply_central_impulse(touched_body.velocity)
