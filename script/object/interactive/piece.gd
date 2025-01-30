8df5f522c59d0d584f800a8ff54df0e055914ac1
TechMan06 <132509305+TechMan06@users.noreply.github.com>
2025-01-30 12:08:46 -0300

    Fixed Piece functionality on 4.4

--- script/object/interactive/piece.gd
+++ script/object/interactive/piece.gd
@@ -9,6 +9,7 @@ const VER_SPEED: int = 5
 var global_data: GlobalData = load("res://resource/management/global_data.tres")
 var current_frame: int = 0
 var collected: bool = false
+var performed_check: bool = false
 var type_array: Array[int] = [
 								0,1,2,3,4,
 								4,0,2,1,3,
@@ -35,14 +36,17 @@ func _ready() -> void:
 		piece_sprite.frame_coords.y = type_array[get_tree().get_nodes_in_group("piece").find(self)]
 	else:
 		piece_sprite.frame_coords.y = randi_range(0, 4)
-	
-	await get_tree().process_frame
-	
-	EventBus.piece_spawned.emit(get_tree().get_nodes_in_group("piece").find(self))
+
+
+func _process(_delta) -> void:
+	if !performed_check:
+		if get_tree().get_nodes_in_group("piece").find(self) != null:
+			EventBus.piece_spawned.emit(get_tree().get_nodes_in_group("piece").find(self))
+			performed_check = true
 
 
 func _on_body_entered(body: Node3D) -> void:
-	if body is Player:
+	if body is Player && performed_check:
 		piece_sound.play()
 		
 		EventBus.piece_collected.emit(get_tree().get_nodes_in_group("piece").find(self))
